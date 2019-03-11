28648497b399f6ddd824d58fb4d64024ab1cf509
Anit Shrestha Manandhar <shrestha.anit@gmail.com>
2019-03-11 18:26:46 +0545

    #HackerRank #Problem:Anagram. Two words are anagrams of one another if their letters can be rearranged to form the other word. Resolved the problem.

--- src/hackerrank/RansomeNoteMain.java
+++ src/hackerrank/RansomeNoteMain.java
@@ -6,7 +6,16 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-
+/**
+ * Problem Definition:
+ *
+ * https://www.hackerrank.com/challenges/ctci-ransom-note/
+ *
+ * Harold is a kidnapper who wrote a ransom note, but now he is worried it will be traced back to him through his handwriting. He found a magazine and wants to know if he can cut out whole words from it and use them to create an untraceable replica of his ransom note. The words in his note are case-sensitive and he must use only whole words available in the magazine. He cannot use substrings or concatenation to create the words he needs.
+ * Given the words in the magazine and the words in the ransom note, print Yes if he can replicate his ransom note exactly using whole words from the magazine; otherwise, print No.
+ * For example, the note is "Attack at dawn". The magazine contains only "attack at dawn". The magazine has all the right words, but there's a case mismatch. The answer is NO.
+ *
+ */
 public class RansomeNoteMain {
 
     public static void main(String args[]) {
--- src/hackerrank/TwoString.java
+++ src/hackerrank/TwoString.java
@@ -3,6 +3,16 @@ package hackerrank;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Problem Definition:
+ *
+ * https://www.hackerrank.com/challenges/two-strings/
+ *
+ * Given two strings, determine if they share a common substring. A substring may be as small as one character.
+ * For example, the words "a", "and", "art" share the common substring a.
+ * The words "be" and "cat" do not share a substring.
+ *
+ */
 public class TwoString {
 
     public static void main(String args[]) {
