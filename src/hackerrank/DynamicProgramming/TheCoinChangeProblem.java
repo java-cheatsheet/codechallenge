c8176d57e95dca0228db52779f129bdf6deb1b16
Anit Shrestha Manandhar <shrestha.anit@gmail.com>
2019-03-31 16:03:10 +0545

    #note: Added resources gathered in the process of understanding dynamic programming concept. #hackerrank #problem:TheCoinChange Implemented the solution watching the video by Hackerrank, link commeted. I still have not figured out the solution implementation completely. Looking forward to the understanding more in the other exercies. For the better understanding, also found out that I needed to improve a bit more on the recursive programming and Greeady approach, and to understand when to use what type of alogorithms and how to find out those critera.

--- src/hackerrank/DynamicProgramming/TheCoinChangeProblem.java
+++ src/hackerrank/DynamicProgramming/TheCoinChangeProblem.java
@@ -1,24 +1,115 @@
 package hackerrank.DynamicProgramming;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
+/**
+ * problem: https://www.hackerrank.com/challenges/coin-change/problem
+ *
+ * solution:
+ * The problem is resolved with the code from the video below:
+ * https://www.youtube.com/watch?v=sn0DWI-JdNA
+ *
+ * The problem itself had another issue with output, "~ no response on stdout ~"
+ * is because the problem required the solution to write with other modes. The
+ * main method had provided hint at the tope with code:
+ *
+ * BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+ *
+ * So it required to use this method to print the answered returned to the main
+ * method and resolved the issue with code:
+ *
+ * bufferedWriter.write(String.valueOf(ways));
+ *
+ * Still I am not very much comfortable with the implementation  of the
+ * GayleMcDowellSolution recursion!
+ *
+ */
 public class TheCoinChangeProblem {
 
     // Driver program to test above function
-    public static void main(String[] args)
-    {
-        int arr[] = {1, 2, 3};
-        int m = arr.length;
-        System.out.println( countTabulated(arr, m, 4) );
+    public static void main(String[] args) {
 
+//        RepresentingNCents objRNC = new RepresentingNCents();
+//        objRNC.makeChange(10);
+//        System.out.println( countTabulated(c, c.length, n) );
+
+        validGMSol3Ways();
     }
 
-    public static int countTabulated( int c[], int m, int n )
-    {
+    public static void validGMSol3Ways () {
+        long c[] = {8, 3, 1, 2};
+        long n = 3;
+
+        GayleMcDowellSolution solution = new GayleMcDowellSolution();
+        long result = solution.makeChange(n, c);
+        long expected = 3;
+        assert (result == expected) : "Result:" + result +" | Expected:" + expected;
+    }
+
+}
+
+
+class GayleMcDowellSolution {
+
+    public long makeChange(long n, long[] c) {
+        return makeChange(c, n, 0, new HashMap<String, Long>());
+    }
+
+    public long makeChange(long[] coins, long money, int index, HashMap<String, Long> memo) {
+//        System.out.println("Coin: " + coins[index]);
+        System.out.println("Money: " + money);
+        System.out.println("Index: " + index);
+
+        if ( money == 0 ) return 1;
+
+        if ( index >= coins.length ) return 0;
+
+        String key =  money + "-" + index;
+        if(memo.containsKey(key)) return memo.get(key);
+
+        long amountWithCoin = 0;
+        long ways = 0;
+        while (amountWithCoin <= money) {
+            long remaining = money - amountWithCoin;
+            ways += makeChange(coins, remaining, index + 1, memo);
+            amountWithCoin += coins[index];
+            System.out.println("AmountWithCoin: " + amountWithCoin);
+        }
+
+        memo.put(key, ways);
+        return ways;
+    }
+}
+
+class OtherWays {
+//    static long getWaysA(long n, long[] c) {
+//        int N = 255;
+//        int m = c.length;
+//        long dp[] = new long[N];
+//        // Base case (If given value is 0)
+//        dp[0] = 1L;
+//
+//        // Pick all coins one by one and update the table[] values
+//        // after the index greater than or equal to the value of the
+//        // picked coin
+//        for( long i=0; i < m; i++) {
+//
+//            for (long j = c[i]; j <= n; j++) {
+//                long interMSum = j - c[i];
+//                //table[j] = table[ i - 1 ] + table[ interMSum ];
+//                dp[j] += dp[interMSum];
+//            }
+//        }
+//        return dp[n];
+//
+//    }
+
+    public static long countTabulated( long c[], int m, long n ) {
         // table[i] will be storing the number of solutions for
         // value i. We need n+1 rows as the table is constructed
         // in bottom up manner using the base case (n = 0)
-        int table[]=new int[n+1];
+        long table[] = new long[(int) n + 1];
 
         // Base case (If given value is 0)
         table[0] = 1;
@@ -26,13 +117,48 @@ public class TheCoinChangeProblem {
         // Pick all coins one by one and update the table[] values
         // after the index greater than or equal to the value of the
         // picked coin
-        for( int i = 0; i<m; i++ )
-            for( int j = c[i]; j <= n; j++)
-                table[j] += table[j - c[i]];
+        for (int i = 0; i < m; i++) {
+            System.out.println("i: " + i);
 
-        return table[n];
+            for (long j = c[i]; j <= n; j++) {
+
+                System.out.println("j: " + (int)j);
+                long aa = j - c[i];
+                table[(int) j] += table[(int) aa];
+
+                System.out.println("table: " + table[(int) j]);
+            }
+        }
+
+
+        return table[(int)n];
     }
 
+    static int countX( long S[], long m, long n )
+    {
+        // If n is 0 then there is 1 solution
+        // (do not include any coin)
+        if (n == 0)
+            return 1;
+
+        // If n is less than 0 then no
+        // solution exists
+        if (n < 0)
+            return 0;
+
+        // If there are no coins and n
+        // is greater than 0, then no
+        // solution exist
+        if (m <=0 && n >= 1)
+            return 0;
+
+        // count is sum of solutions
+        // (i) including S[m-1]
+        // (ii) excluding S[m-1]
+
+        System.out.println(m + " m:n " + n);
+        return ( countX( S, m - 1, n ) + countX( S, m, n - S[(int)m - 1] ) );
+    }
 
     // Returns the count of ways we can
     // sum S[0...m-1] coins to get sum n
@@ -54,11 +180,40 @@ public class TheCoinChangeProblem {
         if (m <=0 && n >= 1)
             return 0;
 
-        // count is sum of solutions (i)
-        // including S[m-1] (ii) excluding S[m-1]
-        return count( S, m - 1, n ) +
-                count( S, m, n-S[m-1] );
+        // count is sum of solutions
+        // (i) including S[m-1]
+        // (ii) excluding S[m-1]
+        return count( S, m - 1, n ) + count( S, m, n - S[m-1] );
     }
 
 
 }
+
+
+class RepresentingNCents {
+
+    int makeChange(int n) {
+        int[] denoms =  {2, 5, 3, 6};//{25, 10, 5, 1};
+        int[][] map = new int[n + 1][denoms.length]; //precomputed vals
+        return makeChange(n, denoms, 0, map);
+    }
+
+    int makeChange(int amount, int[] denoms, int index, int[][] map) {
+        if (map[amount][index] > 0) { //retrieve value
+            return map[amount][index];
+        }
+        if (index >= denoms.length - 1) return 1; //one denom remaining
+
+        int denomAmount = denoms[index];
+        int ways = 0;
+
+        for (int i = 0; i * denomAmount <= amount; i++) {
+            //go to next denom, assuming i coints of denomAmount
+            int amountRemaining = amount - i * denomAmount;
+            ways += makeChange(amountRemaining, denoms, index + 1, map);
+        }
+        map[amount][index] = ways;
+        return ways;
+    }
+}
+
