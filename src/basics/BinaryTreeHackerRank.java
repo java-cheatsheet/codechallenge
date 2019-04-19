6cae45a7f5edb4e7f1fc4ccf31e2c5ac676ad193
Anit Shrestha Manandhar <shrestha.anit@gmail.com>
2019-04-19 12:36:26 +0545

    #hackerrank #LinkedList #BinarySearchTree

---
+++ src/basics/BinaryTreeHackerRank.java
@@ -0,0 +1,129 @@
+package basics;
+
+import java.util.Scanner;
+import java.util.*;
+
+/**
+ * Depth First Traversals:
+ * (a) Inorder (Left, Root, Right)
+ * (b) Preorder (Root, Left, Right)
+ * (c) Postorder (Left, Right, Root)
+ *
+ * Breadth First or Level Order Traversal : 1 2 3 4 5
+ *
+ * https://en.wikipedia.org/wiki/Depth-first_search
+ * https://en.wikipedia.org/wiki/Breadth-first_search
+ *
+ * @param <T>
+ */
+class BinaryTreeHackerRank<T extends Comparable<T>> {
+    /** A reference to the BinaryTreeHackerRank's left subtree. **/
+    BinaryTreeHackerRank<T> left;
+    /** A reference to the BinaryTreeHackerRank's right subtree. **/
+    BinaryTreeHackerRank<T> right;
+    /** The BinaryTreeHackerRank's contents. **/
+    T data;
+
+    /** Construct a BinaryTreeHackerRank object.
+     @param data The BinaryTreeHackerRank's contents.
+     **/
+    public BinaryTreeHackerRank(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    /** Insert new values into the tree.
+     @param value The data value for the BinaryTreeHackerRank to insert.
+     **/
+    public void insert(T value) {
+        // value <= data
+        if(value.compareTo(data) <= 0) {
+            if(left == null) {
+                // Insert at empty subtree
+                left = new BinaryTreeHackerRank<T>(value);
+            }
+            else {
+                // Continue searching for empty subtree
+                left.insert(value);
+            }
+        }
+        else { // value > data
+            if(right == null) {
+                // Insert at empty subtree
+                right = new BinaryTreeHackerRank<T>(value);
+            }
+            else {
+                // Continue searching for empty subtree
+                right.insert(value);
+            }
+        }
+    }
+
+    /** Determines if the tree contains a specific value.
+     @param value The value to search the tree for.
+     @return True if tree contains value; otherwise, false.
+     **/
+    public boolean contains(T value) {
+        if(value.compareTo(data) == 0) {
+            // Value found
+            return true;
+        }
+        else if(value.compareTo(data) < 0) {
+            return (left == null)
+                    ? false // Not found; there are no more BinaryTreeHackerRanks in left subtree to check
+                    : left.contains(value); // Continue searching left subtree
+        }
+        else { // data.compareTo(value) > 0
+            return (right == null)
+                    ? false // Not found; there are no more BinaryTreeHackerRanks in right subrtree to check
+                    : right.contains(value); // Continue searching right subtree
+        }
+    }
+
+
+
+    /** Print InOrder traversal of tree **/
+    public void printInOrder() {
+        if(left != null) {
+            left.printInOrder();
+        }
+
+        System.out.print(data + " ");
+
+        if(right != null) {
+            right.printInOrder();
+        }
+    }
+
+    public static void main(String[] args) {
+        validTestCaseExample1();
+    }
+
+    public static void testGetDataFromCLI() {
+        Scanner scanner = new Scanner(System.in);
+
+        BinaryTreeHackerRank<Integer> root = new BinaryTreeHackerRank<Integer>(scanner.nextInt());
+        while(scanner.hasNextInt()) {
+            int value = scanner.nextInt();
+            root.insert(value);
+        }
+        scanner.close();
+
+        root.printInOrder();
+
+    }
+
+    static void validTestCaseExample1() {
+        int[] nodes = { 4, 2, 3, 1, 7, 6, 5 };
+        int n1 = 1;
+        int n2 = 7;
+
+        BinaryTreeHackerRank<Integer> root = new BinaryTreeHackerRank<>(nodes[0]);
+        for( int i = 1; i < nodes.length; i++ ) {
+            root.insert(nodes[i]);
+        }
+
+        root.printInOrder();
+    }
+}
---
+++ src/basics/SinglyLinkedListImpl.java
@@ -0,0 +1,159 @@
+package basics;
+
+class Node<T> implements Comparable<T> {
+
+    private T value;
+    private Node<T> nextRef;
+
+    public T getValue() {
+        return value;
+    }
+    public void setValue(T value) {
+        this.value = value;
+    }
+    public Node<T> getNextRef() {
+        return nextRef;
+    }
+    public void setNextRef(Node<T> ref) {
+        this.nextRef = ref;
+    }
+    @Override
+    public int compareTo(T arg) {
+        if(arg == this.value){
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
+
+
+public class SinglyLinkedListImpl<T> {
+
+    private Node<T> head;
+    private Node<T> tail;
+
+    public void add(T element){
+
+        Node<T> nd = new Node<T>();
+        nd.setValue(element);
+        System.out.println("Adding: "+element);
+        /**
+         * check if the list is empty
+         */
+        if(head == null){
+            //since there is only one element, both head and
+            //tail points to the same object.
+            head = nd;
+            tail = nd;
+        } else {
+            //set current tail next link to new node
+            tail.setNextRef(nd);
+            //set tail as newly created node
+            tail = nd;
+        }
+    }
+
+    public void addAfter(T element, T after){
+
+        Node<T> tmp = head;
+        Node<T> refNode = null;
+        System.out.println("Traversing to all nodes..");
+        /**
+         * Traverse till given element
+         */
+        while(true){
+            if(tmp == null){
+                break;
+            }
+            if(tmp.compareTo(after) == 0){
+                //found the target node, add after this node
+                refNode = tmp;
+                break;
+            }
+            tmp = tmp.getNextRef();
+        }
+        if(refNode != null){
+            //add element after the target node
+            Node<T> nd = new Node<T>();
+            nd.setValue(element);
+            nd.setNextRef(tmp.getNextRef());
+            if(tmp == tail){
+                tail = nd;
+            }
+            tmp.setNextRef(nd);
+
+        } else {
+            System.out.println("Unable to find the given element...");
+        }
+    }
+
+    public void deleteFront(){
+
+        if(head == null){
+            System.out.println("Underflow...");
+        }
+        Node<T> tmp = head;
+        head = tmp.getNextRef();
+        if(head == null){
+            tail = null;
+        }
+        System.out.println("Deleted: "+tmp.getValue());
+    }
+
+    public void deleteAfter(T after){
+
+        Node<T> tmp = head;
+        Node<T> refNode = null;
+        System.out.println("Traversing to all nodes..");
+        /**
+         * Traverse till given element
+         */
+        while(true){
+            if(tmp == null){
+                break;
+            }
+            if(tmp.compareTo(after) == 0){
+                //found the target node, add after this node
+                refNode = tmp;
+                break;
+            }
+            tmp = tmp.getNextRef();
+        }
+        if(refNode != null){
+            tmp = refNode.getNextRef();
+            refNode.setNextRef(tmp.getNextRef());
+            if(refNode.getNextRef() == null){
+                tail = refNode;
+            }
+            System.out.println("Deleted: "+tmp.getValue());
+        } else {
+            System.out.println("Unable to find the given element...");
+        }
+    }
+
+    public void traverse(){
+
+        Node<T> tmp = head;
+        while(true){
+            if(tmp == null){
+                break;
+            }
+            System.out.println(tmp.getValue());
+            tmp = tmp.getNextRef();
+        }
+    }
+
+    public static void main(String a[]){
+        SinglyLinkedListImpl<Integer> sl = new SinglyLinkedListImpl<Integer>();
+        sl.add(3);
+        sl.add(32);
+        sl.add(54);
+        sl.add(89);
+        sl.addAfter(76, 54);
+        sl.deleteFront();
+        sl.deleteAfter(76);
+        sl.traverse();
+
+    }
+}
