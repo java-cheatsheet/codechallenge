14c3961ab0c908cdba22fa13622179956c124756
Anit <codeanit@gmail.com>
2020-07-23 22:02:02 +0545

    Solved reverse string example.

---
+++ src/basics/ReverseString.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        reverseStringUsingByte();
+    }
+
+    public static void reverseStringUsingByte() {
+        String str = "anit is here";
+        String reversedStr="";
+
+        for(byte c:str.getBytes()){
+            reversedStr = (char)c + reversedStr;
+        }
+        System.out.println("Original String is : " + str);
+        System.out.println("Reversed String is : " + reversedStr);
+    }
+}
