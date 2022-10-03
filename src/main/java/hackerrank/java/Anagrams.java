9c9446a1e1d375f30319147a6af5910658a1edd3
Anit Shrestha Manandhar <codeanit@gmail.com>
2022-10-03 19:51:58 +0400

    Refactor based on all standard rules ./run.sh pmd -d /home/anit/work/codechallenge-java-pom/src/main/java/hackerrank/java/Anagrams.java -f text -R /home/anit/work/codechallenge-java-pom/.pmd --cache /tmp/codechallenge-pmd-analysis | less Remaining issue /home/anit/work/codechallenge-java-pom/src/main/java/hackerrank/java/Anagrams.java:71:  LawOfDemeter:   Potential violation of Law of Demeter (method chain calls)

--- src/main/java/hackerrank/java/Anagrams.java
+++ src/main/java/hackerrank/java/Anagrams.java
@@ -3,35 +3,80 @@ package hackerrank.java;
 import java.util.Arrays;
 import java.util.Locale;
 
+/**
+ * Check if two strings are anagram (case insensitive)
+ *
+ */
 public class Anagrams {
-    String first;
-    String second;
 
-    public Anagrams(String first, String second) {
+    /**
+     * First String
+     */
+    private final String first;
+
+    /**
+     * Second String
+     */
+    private final String second;
+
+    /**
+     * Instantiate first and second strings.
+     *
+     * @param first String
+     * @param second String
+     */
+    public Anagrams(final String first, final String second) {
         this.first =  first;
         this.second =  second;
     }
 
+    public String getFirst() {
+        return first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    /**
+     * Return true if two strings are anagram.
+     *
+     * @return Boolean
+     */
     public boolean isAnagram() {
+        int count = 0;
+        final int firstLen = first.length();
 
-        if ( first.length() != second.length() ) {
-            return false;
-        }
-        Locale locale = Locale.ENGLISH;
-        String firstUpper = first.toUpperCase(locale);
-        String secondUpper = second.toUpperCase(locale);
-        char[] firstCharArr =  firstUpper.toCharArray();
-        char[] secondCharArr =  secondUpper.toCharArray();
-        Arrays.sort(firstCharArr);
-        Arrays.sort(secondCharArr);
+        if ( equal(firstLen, second.length()) ) {
 
-        for (int i=0; i < first.length(); i++) {
+            final char[] firstCharArr = sort(first);
+            final char[] secondCharArr = sort(second);
 
-            if (  firstCharArr[i] != secondCharArr[i] ) {
-                return false;
+            while (count < firstLen) {
+
+                if ( !equal(firstCharArr[count],
+                        secondCharArr[count]) ) {
+                    break;
+                }
+                count++;
             }
         }
 
-        return true;
+        return count == firstLen;
+    }
+
+    private char[] sort(final String randomStr) {
+        final Locale locale = Locale.ENGLISH;
+
+        final char[] upperChar = randomStr.toUpperCase(locale)
+                .toCharArray();
+        Arrays.sort(upperChar);
+
+        return upperChar;
+    }
+
+    private boolean equal(final int first,
+                          final int second) {
+        return first == second;
     }
 }
